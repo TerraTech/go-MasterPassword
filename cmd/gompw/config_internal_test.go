4c05e5db5baf2b6612c5aee20926ab0ab2d45921
core <core@FutureQuest.net>
2017-09-06 01:29:04 -0400

    Add Configuration file handling

---
+++ cmd/gompw/config_internal_test.go
@@ -0,0 +1,62 @@
+//==============================================================================
+// This file is part of go-MasterPassword
+// Copyright (c) 2017, TerraTech
+// Development funded by FutureQuest, Inc.
+//   https://www.FutureQuest.net
+//
+// go-MasterPassword is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-MasterPassword is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find a copy of the GNU General Public License in the
+// LICENSE file.  Alternatively, see <http://www.gnu.org/licenses/>.
+//==============================================================================
+
+package main
+
+import (
+	"testing"
+
+	"futurequest.net/FQgolibs/FQtesting"
+	"github.com/stretchr/testify/assert"
+)
+
+var D = FQtesting.D
+var ane = FQtesting.ANE
+
+func TestGcfn(t *testing.T) {
+	expected := []string{"gompw.toml", "TESTHOME/.gompw.toml", "/etc/gompw.toml"}
+
+	abort := make(chan struct{})
+	defer close(abort)
+	ch := Gcfn(DefaultConfigFilename, "TESTHOME", abort)
+	var i = 0
+	for cf := range ch {
+		if !assert.Equal(t, expected[i], cf) {
+			return
+		}
+		i++
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	var c Config
+
+	expected := Config{
+		Fullname: "TestUser",
+		Password: "liveLifeToTheEdge",
+		PWtype:   "maximum",
+		Site:     "FutureQuest.net",
+		Counter:  69,
+	}
+
+	err := c.LoadConfig("../../files/gompw.toml")
+	ane(t, err)
+	assert.Equal(t, expected, c)
+}
